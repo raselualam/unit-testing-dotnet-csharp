6583925dee509086bd159fec3a9280d96cd091e2
unknown <raselulalam@gmail.com>
2018-02-04 22:19:45 -0500

    UnitTesting

---
+++ UnitTesting.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
+        {
+            //Arrange
+            var reservation = new Reservation();
+
+            //Act
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+            if (MadeBy == user)
+                return true;
+            return false;
+        }
+    }
+}
